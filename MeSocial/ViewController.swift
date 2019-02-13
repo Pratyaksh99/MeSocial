7d92fcb0b7b6f03b804814f3bc58a48f7142966a
Pratyaksh99 <31634863+Pratyaksh99@users.noreply.github.com>
2019-02-13 18:25:16 -0500

    Initial Commit

---
+++ MeSocial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeSocial
+//
+//  Created by Pratyaksh Motwani on 2/13/19.
+//  Copyright © 2019 Pratyaksh Motwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
